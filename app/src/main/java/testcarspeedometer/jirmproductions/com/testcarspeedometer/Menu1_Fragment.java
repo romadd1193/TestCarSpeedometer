996b29f26d900649055afb8f851de092d47ff838
romadd1193 <romadd1193@gmail.com>
2015-06-08 14:49:19 -0700

    mmkay

--- app/src/main/java/testcarspeedometer/jirmproductions/com/testcarspeedometer/Menu1_Fragment.java
+++ app/src/main/java/testcarspeedometer/jirmproductions/com/testcarspeedometer/Menu1_Fragment.java
@@ -5,44 +5,42 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
 /**
  * Created by romad_000 on 6/7/2015.
  */
 public class Menu1_Fragment extends Fragment implements LocationListener {
-    View rootview;
-    TextView initial;
+    TextView txt;
+    public Menu1_Fragment(){}
 
-    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        rootview = inflater.inflate(R.layout.menu1_layout, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
 
-        initial = (TextView) rootview.findViewById(R.id.initial);
+        View rootView = inflater.inflate(R.layout.menu1_layout, container, false);
 
-        LocationManager lm = (LocationManager) this.getActivity().getSystemService(Context.LOCATION_SERVICE);
+        txt=(TextView)rootView.findViewById(R.id.initial);
+
+        LocationManager lm= (LocationManager)getActivity().getSystemService(Context.LOCATION_SERVICE);
         lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
-        this.onLocationChanged(null);
 
-        return rootview;
+        return rootView;
     }
-
     @Override
     public void onLocationChanged(Location location) {
 
-
-        if (location == null) {
-            initial.setText("0.0");
-        } else {
-            float nCurrentSpeed = location.getSpeed();
-            initial.setText((int) nCurrentSpeed);
+        if(location==null){
+            txt.setText("-.- m/s");
         }
+        else {
+            float nCurrentSpeed=location.getSpeed();
+            txt.setText(nCurrentSpeed+" M/S");
+        }
+
     }
 
     @Override
