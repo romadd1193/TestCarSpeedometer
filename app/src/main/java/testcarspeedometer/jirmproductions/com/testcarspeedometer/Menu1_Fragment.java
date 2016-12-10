70f8c15be39f515d2748a8db3fcbb9dd4ce89182
romadd1193 <romadd1193@gmail.com>
2016-12-10 14:54:35 -0500

    hi

--- app/src/main/java/testcarspeedometer/jirmproductions/com/testcarspeedometer/Menu1_Fragment.java
+++ app/src/main/java/testcarspeedometer/jirmproductions/com/testcarspeedometer/Menu1_Fragment.java
@@ -105,12 +105,7 @@ public class Menu1_Fragment extends Fragment implements LocationListener {
         {
             txtBIG.setText("KPH");
         }
-
-        if(Mirror_setting==true){
-
-        }
-
-
+        
         AdLayout adView = (AdLayout) rootView.findViewById(R.id.adView);
         AdTargetingOptions adOptions = new AdTargetingOptions();
         // Optional: Set ad targeting options here.
